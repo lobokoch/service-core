53f944c1a02915d3e45297d97a892ba6c0b7b3b9
Márcio Koch <lobokoch@gmail.com>
2019-08-26 22:03:32 -0300

    Ajustes no plano de contas CORPORATE.

--- src/main/java/br/com/kerubin/api/servicecore/mail/MailSender.java
+++ src/main/java/br/com/kerubin/api/servicecore/mail/MailSender.java
@@ -1,6 +1,8 @@
 package br.com.kerubin.api.servicecore.mail;
 
+import java.text.MessageFormat;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import javax.inject.Inject;
 import javax.mail.internet.MimeMessage;
@@ -20,22 +22,28 @@ public class MailSender {
 	@Inject
 	private JavaMailSender mailSender;
 
-	public void sendMail(String from, List<String> recipients, String subsject, String message) {
+	public void sendMailAsync(String from, List<String> recipients, String subject, String message) {
+		CompletableFuture.runAsync(() -> sendMail(from, recipients, subject, message));
+	}
+	
+	public void sendMail(String from, List<String> recipients, String subject, String message) {
+		String logHeader = MessageFormat.format("from \"{0}\" to \"{1}\" subject \"{2}\"", from, recipients, subject);
+		
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 
 		MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
 		try {
 			helper.setFrom(from);
 			helper.setTo(recipients.toArray(new String[recipients.size()]));
-			helper.setSubject(subsject);
+			helper.setSubject(subject);
 			helper.setText(message, true);
 			
-			log.info("Sending e-mail to: " + recipients);
+			log.info("Sending e-mail {} ", logHeader);
 			mailSender.send(mimeMessage);			
-			log.info("E-mail sended to: " + recipients);
+			log.info("E-mail sended {} ", logHeader);
 		} catch (Exception e) {
-			log.error("Error sending e-mail to : " + recipients, e);
-			new MailSenderException("Error sending e-mail.");
+			log.error("Error sending e-mail " + logHeader, e);
+			new MailSenderException("Error sending e-mail " + logHeader);
 		}
 
 	}
