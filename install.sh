c4607635e9d7e61709b23e3bedc38e3dfb4b492e
Márcio Koch <lobokoch@gmail.com>
2020-03-07 07:18:42 -0300

    Ajustes no script de install e build

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "KERUBIN ERROR: \"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "KERUBIN FINISHED: last command \"${last_command}\" had exit code $?."' EXIT
 
 echo "Starting build generation..."
 
@@ -15,4 +15,4 @@ mvn clean install
 
 echo "DONE"
 
-read -p "Press any key to exit..."
+#read -p "Press any key to exit..."
